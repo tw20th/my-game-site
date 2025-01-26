ff460a3444a741f2ef243a1b07fefc3efd05836e
tw20th <51616143+tw20th@users.noreply.github.com>
2025-01-26 18:42:18 +0900

    feat: implemented game detail page with dynamic routing

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,10 @@
 const nextConfig = {
   // 設定オプションをここに記述
   reactStrictMode: true, // Reactの厳密モードを有効化
-  swcMinify: true,       // SWCによる最小化を有効化
+  swcMinify: true, // SWCによる最小化を有効化
+  images: {
+    domains: ["media.rawg.io"], // 画像のホスト名を追加
+  },
 };
 
 export default nextConfig;
